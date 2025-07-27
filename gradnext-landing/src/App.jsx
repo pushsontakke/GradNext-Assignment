31f73f11ce7993fc45c47fb89e54e34f516672e2
pushsontakke <pusontakke1@gmail.com>
2025-07-27 14:03:19 +0530

    Navigation and Hero Section added

--- gradnext-landing/src/App.jsx
+++ gradnext-landing/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Hero from "./components/Hero.jsx";
+import Nav from "./components/Navigation/Nav.jsx";
 
 function App() {
 
   return (
-    <>
+    <div className="lg:px-20">
+        <div className="py-4">
+            <Nav/>
+        </div>
         <Hero />
-    </>
+    </div>
   )
 }
 
